d9f55e6b2a0dd744fad10a71d677e6c88c96d4c7
Howard Wallis <howard.wallis@just-eat.com>
2018-02-07 16:07:53 +0000

    Cleanup & don't delete the prompt

--- src/components/term/index.js
+++ src/components/term/index.js
@@ -11,60 +11,16 @@ export default class Term extends Component {
 			<div id='terminal' />
 		);
 	};
-			
+
 	componentDidMount() {
 		Terminal.loadAddon('fullscreen');
-		this.setState({
-			term: new Terminal(),
-			currentLine: ''
-		});
-		var term = this.state.term;
+		var term = new Terminal;
 		term.open(document.getElementById('terminal'), true);
 		term.toggleFullscreen();
-		term.writeln('Hello Wordl');
-		term.write(termUtils.printPrompt());
 
-		console.log("test", termUtils, termUtils.keyHandler);
+		termUtils.init(term);
 
-
-		term.on('refresh', data => {
-			// this.forceUpdate();
-			// console.log('refresh');
-		});
-
-		term.on('lineFeed', () => {
-			console.log('lineFeed', this.state.currentLine);
-			this.setState({
-				currentLine: ''
-			});
-		});
-
-		// term.on('key', (key, e) => {
-		// 	console.log(e.key);
-		// 	switch(e.key) {
-		// 		case 'Enter':
-		// 			term.writeln('');
-		// 			break;
-		// 		case 'Backspace':
-		// 			term.refresh(0,999);
-		// 			console.log("refresh");
-		// 			this.setState({
-		// 				currentLine: this.state.currentLine.slice(0, -1)
-		// 			});
-		// 			break;
-		// 		default:
-		// 			term.write(key);
-		// 			this.setState({
-		// 				currentLine: this.state.currentLine += key
-		// 			});
-		// 			break;
-		// 	}
-		// });
-
-		term.on('key', (key, e) => termUtils.keyHandler(key, e, term));
-
-		term.on('data', data => {
-			// console.log('data', data);
-		});
+		term.on('key', termUtils.keyHandler);
+		term.on('lineFeed', () => {});
 	};
 }
--- src/components/term/termUtils.js
+++ src/components/term/termUtils.js
@@ -1,32 +1,60 @@
-const promptPrefix = "anon@howardwallis.tech"
+console.log('loaded the module')
 
-console.log("loaded the module")
+const promptPrefix = 'anon@howardwallis.tech'
 
 // state
-let currentLine = '';
+let _currentLine;
+let _term;
 
-let printPrompt = text => {
-    return text ? `${promptPrefix} ${text}$ ` : `${promptPrefix}$ `;
+let init = term => {
+    _term = term;
+    _currentLine = '';
+    printPrompt();
 }
 
-let keyHandler = (key, e, term) => {
-    var printable = (
-        !e.altKey && !e.altGraphKey && !e.ctrlKey && !e.metaKey
+let printPrompt = text => {
+    _term.write(text ? `${promptPrefix} ${text}$ ` : `${promptPrefix}$ `);
+}
+
+let keyHandler = (key, e) => {
+    let printable = (
+        !e.altKey && !e.altGraphKey && !e.ctrlKey && !e.metaKey && (
+            (e.keyCode >= 48 && e.keyCode <= 90) // 0-9a-z
+            || (e.keyCode >= 96 && e.keyCode <= 105) // numpad 0-9
+            || (e.keyCode >= 186 && e.keyCode <= 222) // ; - '
+        ) // https://www.cambiaresearch.com/articles/15/javascript-char-codes-key-codes
     );
 
-    if (e.keyCode == 13) {
-        term.write('\r\n' + printPrompt());
-    } else if (e.keyCode == 8) {
-        // Do not delete the prompt
-        // if term.x which doesn't exist any more
-        console.log(term)
-        term.write('\b \b');
-    } else if (printable) {
-        term.write(key);
+    switch (e.keyCode) {
+        case 13: // Enter
+            _term.write('\r\n');
+            handleInput(_currentLine);
+            _currentLine = '';
+            printPrompt();
+            break;
+
+        case 8: // backspace
+            if (_currentLine) {
+                _currentLine = _currentLine.slice(0, -1);
+                _term.write('\b \b');
+            }
+            break;
+
+        default:
+            console.log(e.keyCode, key);
+            if (printable) {
+                _currentLine += key;
+                _term.write(key);
+            }
+            break;
     }
 }
 
-export var termUtils = {
-    printPrompt,
+let handleInput = line => {
+    console.log(line);
+}
+
+export let termUtils = {
+    init,
     keyHandler
 }
